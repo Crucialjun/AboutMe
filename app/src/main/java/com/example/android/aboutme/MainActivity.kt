a9f1320f58b3469605dc4307fcb299acf4f87ba9
Crucialjun <nich.otieno@gmail.com>
2020-01-27 10:48:14 +0300

    Hide Keyboard

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -29,9 +31,11 @@ class MainActivity : AppCompatActivity() {
         editText.visibility = View.GONE
         view.visibility = View.GONE
         nicknameText.visibility=View.VISIBLE
+
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
 
-private fun Button.setOnClickListener(it: Any, any: Any) {
 
-}
